d1dd07371154398a83b618cfca4b0c888108955e
Ramon Klass <tier@schokokeks.org>
2016-07-20 14:06:48 +0200

    release 0.1.2-rc2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'axsemantics-cli',
-    version = '0.1.2-rc1',
+    version = '0.1.2-rc2',
 
     description = 'AXSemantics API client Commandline Interface',
 
